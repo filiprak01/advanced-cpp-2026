89085c31c7b84acc391fe869a4adbc8dac0bba45
Filip <filip.rak.job@gmail.com>
2026-03-29 21:55:41 +0200

    add user repo

---
+++ common/include/common/repos/user_repo.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include <string>
+#include <common/models/user.hpp>
+#include <unordered_map>
+#include <shared_mutex>
+#include <mutex>
+
+class UserRepository
+{
+    UserRepository() = default;
+
+public:
+    // implement check for existing and CRUD operations for users
+    bool addUser(const User &user);
+    bool removeUser(const std::string &username);
+    User getUser(const std::string &username);
+    bool userExists(const std::string &username) const;
+
+private:
+    mutable std::shared_mutex mutex;
+    std::unordered_map<std::string, User> users;
+};
---
+++ common/src/user_repo.cpp
@@ -0,0 +1,37 @@
+#include <common/repos/user_repo.hpp>
+
+bool UserRepository::userExists(const std::string &username) const
+{
+    std::shared_lock<std::shared_mutex> lock(mutex);
+    return users.find(username) != users.end();
+}
+
+bool UserRepository::addUser(const User &user)
+{
+    std::unique_lock<std::shared_mutex> lock(mutex);
+    if (userExists(user.getUsername()))
+    {
+        return false;
+    }
+    users[user.getUsername()] = user;
+    return true;
+}
+bool UserRepository::removeUser(const std::string &username)
+{
+    std::unique_lock<std::shared_mutex> lock(mutex);
+    if (!userExists(username))
+    {
+        return false;
+    }
+    users.erase(username);
+    return true;
+}
+User UserRepository::getUser(const std::string &username)
+{
+    std::shared_lock<std::shared_mutex> lock(mutex);
+    if (!userExists(username))
+    {
+        return User();
+    }
+    return users[username];
+}
